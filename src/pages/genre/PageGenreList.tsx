15a9255d25be7f813d5a4b96fcb0cc6a0a404e1b
Danny França <danny_vcf@hotmail.com>
2022-03-16 14:54:01 -0300

    feat: add genre table page component

---
+++ src/pages/genre/PageGenreList.tsx
@@ -0,0 +1,34 @@
+import { Fab } from "@mui/material";
+import { Box } from "@mui/system";
+import AddIcon from "@mui/icons-material/Add";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+import Page from "@/components/Page";
+import GenreTable from "./GenreTable";
+
+const PageGenreList: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Page title={t("Genre List")}>
+      <Box dir="rtl" py={2}>
+        <Fab
+          title={t("Add Genre")}
+          color="primary"
+          size="small"
+          component={Link}
+          to="genres/create"
+        >
+          <AddIcon />
+        </Fab>
+      </Box>
+      <Box>
+        <GenreTable />
+      </Box>
+    </Page>
+  );
+};
+
+export default PageGenreList;
