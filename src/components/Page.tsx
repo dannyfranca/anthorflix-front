c2a048bcb45deee5ec274b4ab7875abea87884e6
Danny França <danny_vcf@hotmail.com>
2022-03-16 16:09:07 -0300

    feat: document.title in sync with page title

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
@@ -7,6 +7,10 @@ type PageProps = {
 };
 
 const Page: React.FC<PageProps> = ({ title, children }) => {
+  useEffect(() => {
+    document.title = `${title ?? ""} - Anthorflix`;
+  }, [title]);
+
   return (
     <Container>
       <Typography
