9314528947430a619963f59d08385987cccc0f2f
Danny França <danny_vcf@hotmail.com>
2022-03-16 11:16:41 -0300

    feat: add Navbar and routes to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
 import "./i18t";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { theme } from "./config/theme";
+import NavBar from "./components/NavBar";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <div />
+        <NavBar />
+        <Box paddingTop="70px">
+          <AppRouter />
+        </Box>
       </BrowserRouter>
     </ThemeProvider>
   );
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+
+import Page from "@/components/Page";
+
+const Dashboard: React.FC = () => {
+  const { t } = useTranslation();
+  return <Page title={t("Dashboard")} />;
+};
+
+export default Dashboard;
